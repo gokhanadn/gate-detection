25d8d5426db37d9a1c838d6b8cbad9220dfdbbac
gokhanadn <aydingo18@itu.edu.tr>
2020-01-28 20:52:59 +0300

    edited

--- detect.py
+++ detect.py
@@ -4,13 +4,13 @@ import cv2
 import numpy as np
 import operator
 
-def main(img):
+def main(img,low,high):
     # Resize
     img = cv2.resize(img, (640, 360), interpolation = cv2.INTER_CUBIC)
     # Change Colorspace
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # Mask
-    masked = cv2.inRange(img, np.array([5, 5, 55]), np.array([40, 40, 250]))
+    masked = cv2.inRange(hsv, low, high)
     # Erosion
     eroded = cv2.morphologyEx(masked, cv2.MORPH_CLOSE, np.ones((5,5), np.uint8))
 
@@ -43,15 +43,37 @@ def main(img):
 
     # cv2.imwrite("output.png", img)
     cv2.imshow("Output", img)
+    cv2.imshow("eroded",eroded)
 
 # Import Image
-cap = cv2.VideoCapture('test.mp4')
+cap = cv2.VideoCapture('/home/gokhan/Downloads/test.mp4')
+
+def nothing(x):
+    pass
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("Low H", "Tracking", 0, 360, nothing)
+cv2.createTrackbar("Low S", "Tracking", 0, 100, nothing)
+cv2.createTrackbar("Low V", "Tracking", 0, 100, nothing)
+cv2.createTrackbar("High H", "Tracking", 0, 360, nothing)
+cv2.createTrackbar("High S", "Tracking", 0, 100, nothing)
+cv2.createTrackbar("High V", "Tracking", 0, 100, nothing)
+
 
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    l_h = cv2.getTrackbarPos("Low H", "Tracking")
+    l_s = cv2.getTrackbarPos("Low S", "Tracking")
+    l_v = cv2.getTrackbarPos("Low V", "Tracking")
+    h_h = cv2.getTrackbarPos("High H", "Tracking")
+    h_s = cv2.getTrackbarPos("High S", "Tracking")
+    h_v = cv2.getTrackbarPos("High V", "Tracking")
+
+
     if ret == True:
-        main(frame)
+        main(frame,np.array([l_h,l_s,l_v]),np.array([h_h,h_s,h_v]))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
